b64731513dfc0a41633bd71b609a9db32fa1fae2
Morswin <41349347+Morswin@users.noreply.github.com>
2024-03-24 21:36:21 +0100

    Add Point class

---
+++ README.md
@@ -0,0 +1,1 @@
+I just want to visualise something.
--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 import pygame as pg
 
 
+class Point:
+    x: int
+    y: int
+
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+    def get(self) -> (int, int):
+        return (self.x, self.y)
+
+    def get_x(self) -> int:
+        return self.x
+
+    def get_y(self) -> int:
+        return self.y
+
+
 def main():
     pg.init()
-    running = True
+    running: bool = True
     clock = pg.time.Clock()
-    window = pg.display.set_mode((800, 800))
+    window_size = (800, 800)
+    window = pg.display.set_mode(window_size)
     window.fill((255, 255, 255))
+    # 
+    points: [Point] = [
+        Point(100, 100),
+        Point(200, 200),
+        Point(300, 300)
+    ]
     while running:
         clock.tick(60)
         window.fill((0, 0, 0))
-        for event in pg.event.get():
+        for event in pg.event.get():  # Event loop
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     running = False
             if event.type == pg.QUIT:
                 running = False
+        # End of event loop
+        for point in points:
+            pg.draw.circle(window, (127, 127, 127), point.get(), 5)
         pg.display.flip()
     pg.quit()
 
