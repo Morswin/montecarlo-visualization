e7888f27be1cfdebb84cf7ce24490db72b7e7bda
Morswin <41349347+Morswin@users.noreply.github.com>
2024-03-31 17:22:46 +0200

    Colors

--- main.py
+++ main.py
@@ -18,24 +18,26 @@ class Color:
     green: int
     blue: int
 
-    def __init__(self, r, g, b):
+    def __init__(self, r=0, g=0, b=0):
         self.red = r
         self.green = g
         self.blue = b
 
-    def __add__(self, other) -> None:
+    def __add__(self, other):
         if isinstance(other, Color):
             self.red += other.red
             self.green += other.green
             self.blue += other.blue
+            return self
         else:
             raise TypeError("You cannot add to color, comething that isn't another color")
 
-    def __sub__(self, other) -> None:
-        if isistance(other, Color):
+    def __sub__(self, other):
+        if isinstance(other, Color):
             self.red -= other.red
             self.green -= other.green
             self.blue -= other.blue
+            return self
         else:
             raise TypeError("You cannot substract from color, comething that isn't another color")
 
@@ -63,10 +65,10 @@ class Point:
     y: int
     color: Color
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, color=Color()):
         self.x = x
         self.y = y
-        self.color = Color(0, 0, 0)
+        self.color = color
 
     def get(self) -> (int, int):
         return (self.x, self.y)
@@ -91,9 +93,11 @@ def main():
     # 
     program_state = ProgramState()
     points: [Point] = []
-    functions = [
-        lambda point: math.sqrt((point.get_x() - 400)**2 + (point.get_y() - 400)**2) <= 400  # 400 == half of the screen
-    ]
+    functions = []
+    functions.append(lambda point: math.sqrt((point.get_x() - 400)**2 + (point.get_y() - 400)**2) <= 400)  # 400 == half of the screen
+    functions.append(lambda point: math.sqrt((point.get_x() - 400)**2 + (point.get_y() - 400)**2) <= 200)
+    functions.append(lambda point: -point.get_x() + 600 < point.get_y())
+    functions.append(lambda point: -point.get_x() + 1000 > point.get_y())
     while running:
         clock.tick(60)
         window.fill((0, 0, 0))
@@ -107,12 +111,17 @@ def main():
                 running = False
         # End of event loop
         if program_state.is_adding_new_points:
-            points.append(Point(random.randint(0, window_size[0]), random.randint(0, window_size[1])))
+            points.append(Point(random.randint(0, window_size[0]), random.randint(0, window_size[1]), Color(63, 63, 63)))
         for point in points:
             if functions[0](point):
-                pg.draw.circle(window, (255, 255, 255), point.get(), 5)
-            else:
-                pg.draw.circle(window, (127, 127, 127), point.get(), 5)
+                point.color += Color(255, 255, 255)
+            if functions[1](point):
+                point.color += Color(127, 127, 127)
+            if functions[2](point):
+                point.color -= Color(0, 255, 0)
+            if functions[3](point):
+                point.color -= Color(0, 0, 255)
+            pg.draw.circle(window, point.get_color(), point.get(), 5)
         pg.display.flip()
     pg.quit()
 
